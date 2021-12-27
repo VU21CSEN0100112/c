df046e6e94b9233a5598c495fece0c2db4515420
VU21CSEN0100112 <95603456+VU21CSEN0100112@users.noreply.github.com>
2021-12-27 10:12:50 +0530

    Add files via upload

---
+++ product of digits.cpp
@@ -0,0 +1,22 @@
+
+#include<stdio.h> 
+ main()
+{       
+    int num, rem, prod = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        rem = num % 10;
+        prod *= rem; 
+        num /=  10;  
+    }
+
+    printf("%d", prod);
+
+    return 0; 
+}
+
+
