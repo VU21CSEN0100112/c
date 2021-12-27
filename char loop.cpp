8223acc1226f7d6cf8c9233297164ad0301ed53f
VU21CSEN0100112 <95603456+VU21CSEN0100112@users.noreply.github.com>
2021-12-27 10:05:47 +0530

    Add files via upload

---
+++ char loop.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+ main()
+{
+	//loop counter or a variable that
+	//will store initial alphabet,
+	//from where we will print the alphabets
+	char alphabet;
+	//assigning 'A' as initial alphabet
+	alphabet='A';
+
+	//print statement
+	printf("Uppercase alphabets:\n");
+
+	//loop statement, that will check the condition
+	//and print the alphabets from 'A' to 'Z'
+	while(alphabet<='Z')
+	{
+		//printing the alphabets
+		printf("%c ",alphabet);
+		//increasing the value by 1
+		alphabet++;
+	}
+
+	return 0;
+}
+
+
---
+++ uppercase and lowercase alphabets.cpp
@@ -0,0 +1,19 @@
+
+ 
+#include <stdio.h>
+ 
+ main()
+{
+    char i;
+ 
+    printf("Capital (upper) case characters:\n");
+    for(i='A'; i<='Z'; i++)
+        printf("%c ",i);
+ 
+    printf("\n\nLower case characters:\n");
+    for(i='a'; i<='z'; i++)
+        printf("%c ",i);
+     
+    return 0;
+}
+
